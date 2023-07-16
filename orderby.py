0b58e8627dfbb4718002a1ced6ca4de89c8ec220
Pacific <abhishek28c@gmail.com>
2023-07-17 00:55:50 +0530

    order by

---
+++ orderby.py
@@ -0,0 +1,10 @@
+import sqlite3
+# connecting to the database
+conn = sqlite3.connect('data.db')
+# creating cursor
+c = conn.cursor()
+# executing command of order by
+items = c.execute("select * from data where rowid = 3")
+print(items.fetchall())
+conn.commit()
+conn.close()
